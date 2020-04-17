dbdc4d2f6e7bd54f2b278efbb30a82a7b1178ede
chelin <chelin@bcbook.cn>
2020-04-17 12:54:29 +0800

    测试

--- App/modules/company/apis.py
+++ App/modules/company/apis.py
@@ -139,6 +139,8 @@ class CompanySearchApi(Resource):
         _page_num = _total // page_size
         if _total % page_size > 0:
             _page_num += 1
+        print(page_num, type(page_num), 1)
+        print(_page_num, type(_page_num), 2)
         if page_num > _page_num:
             page_num = _page_num
         filter_list = []
